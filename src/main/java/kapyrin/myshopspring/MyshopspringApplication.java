abcc31add1433f77acbe4b7b9efb915c79ed9887
Vladimir Kapyrin <vladimir.kapyrin@gmail.com>
2024-11-14 20:17:14 +0000

    Tests for service and controller layers.

--- src/main/java/kapyrin/myshopspring/MyshopspringApplication.java
+++ src/main/java/kapyrin/myshopspring/MyshopspringApplication.java
@@ -1,13 +1,31 @@
 package kapyrin.myshopspring;
 
-import org.springframework.boot.SpringApplication;
+import kapyrin.myshopspring.entity.User;
+import kapyrin.myshopspring.util.ReportStringGenerator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class MyshopspringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyshopspringApplication.class, args);
+//        SpringApplication.run(MyshopspringApplication.class, args);
+        ReportStringGenerator reportStringGenerator = new ReportStringGenerator();
+
+        User user = User.builder()
+                .firstName("First")
+                .lastName("Lastt")
+                .email("update@email.com")
+                .password("password")
+                .phoneNumber("6789012456")
+                .address("Washington")
+                .build();
+
+        List<User> userList = List.of(user);
+
+        String reportString = reportStringGenerator.fromUserList(userList);
+        System.out.println(reportString);
     }
 
 }
