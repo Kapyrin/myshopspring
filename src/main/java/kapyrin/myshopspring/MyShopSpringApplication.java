ae199bbd4832e65b87a5a5db869cc2ae94d60289
Vladimir Kapyrin <vladimir.kapyrin@gmail.com>
2024-11-15 19:02:11 +0000

    Tests for service and controller layers.

---
+++ src/main/java/kapyrin/myshopspring/MyShopSpringApplication.java
@@ -0,0 +1,14 @@
+package kapyrin.myshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopSpringApplication.class, args);
+
+    }
+
+}
